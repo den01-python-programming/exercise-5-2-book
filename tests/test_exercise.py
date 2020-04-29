9a4ad690b116bc1136071cd9084f3e195a5bf3c1
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-29 10:35:11 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,6 +1,10 @@
 import pytest
-import src.exercise
+from src.book import Book
 
 def test_exercise():
-    #implement tests here
-    assert 0 == 0
+    book = Book("J.R.R. Tolkein", "Lord of the Rings", "1000")
+
+    assert book.get_name() == "J.R.R. Tolkein"
+    assert book.get_pages() == "Lord of the Rings"
+    assert book.get_author() == "1000"
+    assert str(book) == "J.R.R. Tolkein, Lord of the Rings, 1000 pages"
