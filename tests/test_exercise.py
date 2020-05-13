7ebfb5764f878e7af00f895f8993346970ee0dd2
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-13 11:23:34 +0100

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -4,7 +4,7 @@ from src.book import Book
 def test_exercise():
     book = Book("J.R.R. Tolkein", "Lord of the Rings", "1000")
 
-    assert book.get_name() == "J.R.R. Tolkein"
-    assert book.get_pages() == "Lord of the Rings"
-    assert book.get_author() == "1000"
+    assert book.get_author() == "J.R.R. Tolkein"
+    assert book.get_name() == "Lord of the Rings"
+    assert book.get_pages() == "1000"
     assert str(book) == "J.R.R. Tolkein, Lord of the Rings, 1000 pages"
